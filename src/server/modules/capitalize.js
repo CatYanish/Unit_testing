ed61664ce52fe6654699652add8ef5dfd581ad64
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-20 22:02:42 -0500

    tests failing

--- src/server/modules/capitalize.js
+++ src/server/modules/capitalize.js
@@ -1,10 +1,15 @@
 function makeTitle(str) {
-	return str
-        .split(' ')
-        .map(function(word) {
-            return word[0].toUpperCase() + word.substr(1);
-        })
-        .join(' ');
+	if(typeof str == "string") {
+		return str
+		.split(' ')
+		.map(function(word) {
+			return word[0].toUpperCase() + word.substr(1);
+		})
+		.join(' ');
+	} else {
+		throw TypeError('expects a string');
+	}
 }
+
 
 module.exports = makeTitle;
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,13 +1,14 @@
 var expect = require('chai').expect;
 var capitalize = require('../src/server/modules/capitalize');
+var string = "this is an example";
 
 describe('capitalize module', function() {
 
-  it('should capitalize a string passed in', function() {
-    expect(capitalize).to.return(TypeError, /expects a number/);
+  it('should take in a string or error nicely', function() {
+    expect(capitalize(1)).to.throw(TypeError, /expects a string/);
   });
 
   it('should return the string even when passed an even nubmer', function() {
-    expect(oddEven(4)).to.equal('even');
+    expect(capitalize(string)).to.equal('THIS IS AN EXAMPLE');
   });
 });
