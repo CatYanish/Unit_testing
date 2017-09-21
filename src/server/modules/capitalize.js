ead81de64b0e52b42c5d2ceebfaea7dac43f108a
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-20 22:19:52 -0500

    four tests passing

--- src/server/modules/capitalize.js
+++ src/server/modules/capitalize.js
@@ -1,13 +1,13 @@
 function makeTitle(str) {
-	if(typeof str == "string") {
+	if(typeof str !== "string") {
+		throw TypeError('expects a string');
+	} else {
 		return str
 		.split(' ')
 		.map(function(word) {
 			return word[0].toUpperCase() + word.substr(1);
 		})
 		.join(' ');
-	} else {
-		throw TypeError('expects a string');
 	}
 }
 
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -4,11 +4,11 @@ var string = "this is an example";
 
 describe('capitalize module', function() {
 
-  it('should take in a string or error nicely', function() {
-    expect(capitalize(1)).to.throw(TypeError, /expects a string/);
+  it('should take error if passed a non-string', function() {
+    expect(capitalize).to.throw(TypeError, /expects a string/);
   });
 
   it('should return the string even when passed an even nubmer', function() {
-    expect(capitalize(string)).to.equal('THIS IS AN EXAMPLE');
+    expect(capitalize(string)).to.equal('This Is An Example');
   });
 });
